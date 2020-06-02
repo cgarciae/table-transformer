eb5f8feddd162ba87bd7c82de6c78cd4d6fef53c
Cristian Garcia <cgarcia.e88@gmail.com>
2020-06-02 09:20:35 -0500

    init

--- training/estimator.py
+++ training/estimator.py
@@ -3,17 +3,14 @@ import tensorflow_addons as tfa
 from imblearn.over_sampling import RandomOverSampler
 import pandas as pd
 from pathlib import Path
+import typing as tp
 import numpy as np
+from sklearn.model_selection import train_test_split
 import shutil
-import cv2
 
 
 def split(df, params):
-
-    n_split = int(params.train_split * len(df))
-
-    df_train = df[:n_split]
-    df_test = df[n_split:]
+    df_train, df_test = train_test_split(df, test_size=params.train_split)
 
     return df_train, df_test
 
@@ -71,51 +68,16 @@ def preprocess(df, params, mode):
 
 
 def get_dataset(df, params, mode):
-    dataset = tf.data.Dataset.from_tensor_slices(df.to_dict(orient="list"))
+    dataset = tf.data.Dataset.from_tensor_slices(
+        {col: df[col].to_numpy() for col in df}
+    )
 
     if mode == "train":
         dataset = dataset.repeat()
 
-        # def filter_fn(row):
-        #     return tf.math.not_equal(row["original_steering"], 0) | (
-        #         tf.random.uniform(shape=()) < params.percent_zero
-        #     )
-
-        # dataset = dataset.filter(filter_fn)
-
-    def preprocess_image(x):
-        x = x[params.crop_up : -params.crop_down, :, :]
-        x = cv2.resize(x, tuple(params.image_size[::-1]))
-        x = cv2.cvtColor(x, cv2.COLOR_RGB2YUV).astype(np.float32) / 255.0
-
-        return x
-
-    def load_row(row):
-        image = tf.io.read_file(row["image_path"])
-        image = tf.io.decode_image(image)
-
-        if tf.equal(row["flipped"], 1):
-            image = tf.image.flip_left_right(image)
-
-        image = tf.numpy_function(preprocess_image, [image], tf.float32)
-        image.set_shape((params.image_size[0], params.image_size[1], 3))
-        steering = row["steering"] + tf.random.normal(
-            shape=(), stddev=params.steering_std
-        )
-
-        if mode == "train":
-            if tf.equal(row["original_steering"], 0):
-                sample_weight = params.percent_zero
-            else:
-                sample_weight = 1.0
-
-            return image, row["steering"], sample_weight
-        else:
-            return image, row["steering"]
-
-    dataset = dataset.map(load_row, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.shuffle(100)
 
+    dataset = dataset.map(lambda d: (dict(x0=d["x0"], x1=d["x1"]), d["y"]))
     dataset = dataset.batch(params.batch_size, drop_remainder=True)
     dataset = dataset.prefetch(1)
 
@@ -124,46 +86,122 @@ def get_dataset(df, params, mode):
 
 def get_model(params) -> tf.keras.Model:
 
-    x = inputs = tf.keras.Input(
-        shape=(params.image_size[0], params.image_size[1], 3), name="image"
-    )
+    x0 = tf.keras.Input(shape=(1,), name="x0")
+    x1 = tf.keras.Input(shape=(1,), name="x1")
 
-    x = tf.keras.layers.Conv2D(24, [5, 5], strides=2)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    inputs = [x0, x1]
 
-    x = tf.keras.layers.Conv2D(36, [5, 5], strides=2)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    # x0 embeddings
 
-    x = tf.keras.layers.Conv2D(48, [5, 5], strides=2)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    # x0 = tf.keras.layers.Dense(10, activation="relu")(x0)
+    # x0 = x0[:, None, :]
 
-    x = tf.keras.layers.Conv2D(64, [3, 3])(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    # x1 = tf.keras.layers.Dense(10, activation="relu")(x1)
+    # x1 = x1[:, None, :]
 
-    x = tf.keras.layers.Conv2D(64, [3, 3])(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    x = tf.concat([x0, x1], axis=1)
 
-    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(512, activation="relu")(x)
+    x = tf.keras.layers.Dense(512, activation="relu")(x)
 
-    x = tf.keras.layers.Dense(100)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    # x = AddPositionalEmbeddings()(x)
+    # x = SelfAttentionBlock(10, head_size=10, num_heads=8)(x)
+    # x = SelfAttentionBlock(10, head_size=10, num_heads=8)(x)
+    # x = SelfAttentionBlock(10, head_size=10, num_heads=8)(x)
+    # x = AttentionPooling(10, n_queries=1, head_size=10, num_heads=8)(x)
 
-    x = tf.keras.layers.Dense(50)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    # x = x[:, 0]
+    # x = tf.keras.layers.GlobalAveragePooling1D()(x)
 
-    x = tf.keras.layers.Dense(10)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.nn.relu(x)
+    x = tf.keras.layers.Dense(1, activation="sigmoid", name="y")(x)
 
-    x = tf.keras.layers.Dense(1, name="steering")(x)
-
-    model = tf.keras.Model(inputs=inputs, outputs=x, name="nvidia_net")
+    model = tf.keras.Model(inputs=inputs, outputs=x, name="tabular_attention")
 
     return model
+
+
+class AddPositionalEmbeddings(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.embeddings: tp.Optional[tf.Variable] = None
+
+    def build(self, input_shape):
+
+        input_shape = list(input_shape)
+
+        self.embeddings = self.add_weight(
+            name="key_kernel", shape=[1] + input_shape[1:]
+        )
+
+        super().build(input_shape)
+
+    def call(self, inputs):
+        return inputs + self.embeddings
+
+
+class SelfAttentionBlock(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        output_size: int,
+        head_size: int = 16,
+        num_heads: int = 3,
+        dropout: float = 0.0,
+        activation: tp.Union["str", tp.Callable] = "relu",
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.mha = tfa.layers.MultiHeadAttention(
+            head_size=head_size, num_heads=num_heads, dropout=dropout
+        )
+        self.dense = tf.keras.layers.Dense(output_size, activation=activation)
+
+    def call(self, inputs):
+
+        x = self.mha([inputs, inputs])
+        x = self.dense(x)
+
+        return x
+
+
+class AttentionPooling(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        output_size: int,
+        n_queries: int,
+        head_size: int = 16,
+        num_heads: int = 3,
+        dropout: float = 0.0,
+        activation: tp.Union["str", tp.Callable] = "relu",
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.n_queries = n_queries
+        self.mha = tfa.layers.MultiHeadAttention(
+            head_size=head_size, num_heads=num_heads, dropout=dropout
+        )
+        self.dense = tf.keras.layers.Dense(output_size, activation=activation)
+        self.query: tp.Optional[tf.Variable] = None
+
+    def build(self, input_shape):
+
+        num_features = input_shape[-1]
+
+        self.query = self.add_weight(
+            name="key_kernel", shape=[1, self.n_queries, num_features]
+        )
+
+        super().build(input_shape)
+
+    def call(self, inputs):
+
+        query = tf.tile(
+            self.query, [tf.shape(inputs)[0]] + [1] * (len(inputs.shape) - 1)
+        )
+
+        x = self.mha([query, inputs])
+        x = self.dense(x)
+
+        return x
--- training/experiment.py
+++ training/experiment.py
@@ -11,68 +11,45 @@ import pandas as pd
 import typer
 import tensorflow as tf
 import numpy as np
+from plotly import graph_objects as go
 
 # from .
-import estimator
+from . import estimator
 
 
 def main(
     params_path: Path = Path("training/params.yml"),
     cache: bool = False,
     viz: bool = False,
+    debug: bool = False,
 ):
+    if debug:
+        import debugpy
+
+        print("Waiting debuger....")
+        debugpy.listen(("localhost", 5678))
+        debugpy.wait_for_client()
 
     params = dicto.load(params_path)
 
-    train_cache_path = Path("cache") / "train.feather"
-    test_cache_path = Path("cache") / "test.feather"
-
-    if cache and train_cache_path.exists() and test_cache_path.exists():
-        print("Using cache...")
-
-        df_train = pd.read_feather(train_cache_path)
-        df_test = pd.read_feather(test_cache_path)
-
-    else:
-        df = dataget.image.udacity_simulator().get()
-
-        df_train, df_test = estimator.split(df, params)
-
-        df_train = estimator.preprocess(df_train, params, "train")
-        df_test = estimator.preprocess(df_test, params, "test")
-
-        # cache data
-        df_train = df_train.reset_index(drop=True)
-        df_test = df_test.reset_index(drop=True)
-
-        train_cache_path.parent.mkdir(exist_ok=True)
-
-        df_train.to_feather(train_cache_path)
-        df_test.to_feather(test_cache_path)
+    df_train, df_test = dataget.kaggle(competition="cat-in-the-dat-ii").get(
+        files=["train.csv", "test.csv"]
+    )
 
     ds_train = estimator.get_dataset(df_train, params, "train")
     ds_test = estimator.get_dataset(df_test, params, "test")
 
-    # Visualize dataset for debuggings
-    if viz:
-        import matplotlib.pyplot as plt
-
-        iteraror = iter(ds_train)
-        image_batch, steer_batch = next(iteraror)
-        for image, steer in zip(image_batch, steer_batch):
-            plt.imshow(image.numpy().astype(np.uint8))
-            plt.title(f"Steering angle: {steer}")
-            plt.show()
-
-        return
-
     model = estimator.get_model(params)
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(params.lr), loss="mse", metrics=["mae"],
+        optimizer=tf.keras.optimizers.Adam(params.lr),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
     )
 
     model.summary()
+
+    print(ds_train)
 
     model.fit(
         ds_train,
@@ -89,6 +66,67 @@ def main(
     # Export to saved model
     save_path = f"models/{model.name}"
     model.save(save_path)
+
+    print(f"{save_path=}")
+
+    vizualize(df_train, df_test, model)
+
+
+def vizualize(df_train, df_test, model):
+    fig = go.Figure(
+        [
+            go.Scatter(
+                x=df_train.x0,
+                y=df_train.x1,
+                marker=go.scatter.Marker(
+                    color=df_train.y, size=8, line_width=2, line_color="DarkSlateGrey",
+                ),
+                mode="markers",
+                name="train",
+            ),
+            go.Scatter(
+                x=df_test.x0,
+                y=df_test.x1,
+                marker=go.scatter.Marker(
+                    color=df_test.y, size=12, line_width=2, line_color="DarkSlateGrey",
+                ),
+                mode="markers",
+                name="test",
+            ),
+        ]
+    )
+    fig.update_layout(template="simple_white")
+    xx, yy, zz = decision_boundaries(df_train[["x0", "x1"]].to_numpy(), model)
+    xx = xx[0]
+    yy = yy[:, 0]
+    fig.add_trace(go.Contour(x=xx, y=yy, z=zz, opacity=0.5))
+    fig.show()
+
+
+def decision_boundaries(X, model, n=20):
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    print(x_min, x_max)
+    print(y_min, y_max)
+
+    hx = (x_max - x_min) / n
+    hy = (y_max - y_min) / n
+    xx, yy = np.meshgrid(
+        np.arange(x_min, x_max + hx, hx), np.arange(y_min, y_max + hy, hy)
+    )
+
+    # Obtain labels for each point in mesh using the model.
+    points = np.stack([xx.ravel(), yy.ravel()], axis=1)
+
+    Z = model(dict(x0=points[:, 0], x1=points[:, 1])).numpy()
+    Z = (Z > 0.5).astype(np.int32)
+
+    zz = Z.reshape(xx.shape)
+
+    return xx, yy, zz
 
 
 if __name__ == "__main__":
